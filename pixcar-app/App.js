430b15269098b2467b71e2fd6378154b113a8550
BautiM37 <bmigliore@udesa.edu.ar>
2022-11-07 12:27:30 -0300

    inicializacion mi perfil y tab navigation

--- pixcar-app/App.js
+++ pixcar-app/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// importar vistas
+import MiPerfil from './src/screens/MiPerfil';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <MiPerfil />
+   
+  
   );
 }
 
---
+++ pixcar-app/src/screens/MiPerfil.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+class MiPerfil extends Component {
+    constructor() {
+        super()
+        this.state={
+
+        }
+    }
+
+    render() {
+        return(
+            <View>
+                <Text>Mi Perfil</Text>
+            </View>
+        )
+    }
+
+}
+
+export default MiPerfil;
